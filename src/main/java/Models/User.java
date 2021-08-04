21d5ba575eadb7b2c75cc4f7d8d7874c39a3d386
Shoreniko <sjanj19@freeuni.edu.ge>
2021-08-04 22:40:18 +0400

    added user model class with some methods yet to be implemented

---
+++ src/main/java/Models/User.java
@@ -0,0 +1,88 @@
+package Models;
+
+import java.util.List;
+
+public class User {
+    // Instance variable declarations.
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+
+    // Constructor.
+    public User(String username, String password, String firstName, String lastName, String dateOfBirth){
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public List<Badge> getBadges(){
+        // TO BE IMPLEMENTED
+        return null;
+    }
+
+    public List<Item> getItems(){
+        // TO BE IMPLEMENTED
+        return null;
+    }
+
+    public List<Review> getReviews(){
+        // TO BE IMPLEMENTED
+        return null;
+    }
+
+    public List<User> getFollowers(){
+        // TO BE IMPLEMENTED
+        return null;
+    }
+
+    public List<User> getFollowing(){
+        // TO BE IMPLEMENTED
+        return null;
+    }
+
+    // Getter methods.
+    public String getUsername(){
+        return username;
+    }
+
+    public String password(){
+        return password;
+    }
+
+    public String firstName(){
+        return firstName;
+    }
+
+    public String lastName(){
+        return lastName;
+    }
+
+    public String dateOfBirth(){
+        return dateOfBirth;
+    }
+
+    // Setter methods.
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public void setDateOfBirth(String dateOfBirth){
+        this.dateOfBirth = dateOfBirth;
+    }
+}
