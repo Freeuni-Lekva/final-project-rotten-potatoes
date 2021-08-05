4e9fdc757c4b1fe53e200aa7d9ad312dd7935631
Shoreniko <sjanj19@freeuni.edu.ge>
2021-08-05 13:08:09 +0400

    added necessary function declarations for review model class

--- src/main/java/Models/Review.java
+++ src/main/java/Models/Review.java
@@ -1,5 +1,18 @@
 package Models;
 
 public class Review {
-    
+    public Item getItem(){
+        // IMPLEMENT
+        return null;
+    }
+
+    public String getUserReview(){
+        // IMPLEMENT
+        return null;
+    }
+
+    public int getUserScore(){
+        // IMPLEMENT
+        return -1;
+    }
 }
