d74df1b7df37eaec3b702f83a8c4b83695afbb6d
Shoreniko <sjanj19@freeuni.edu.ge>
2021-08-05 12:56:24 +0400

    added necessary function declarations

--- src/main/java/Models/Item.java
+++ src/main/java/Models/Item.java
@@ -1,5 +1,18 @@
 package Models;
 
 public class Item {
+    public String getTitle(){
+        // IMPLEMENT
+        return null;
+    }
 
+    public String getCoverURL(){
+        // IMPLEMENT
+        return null;
+    }
+
+    public int getScore(){
+        // IMPLEMENT
+        return -1;
+    }
 }
