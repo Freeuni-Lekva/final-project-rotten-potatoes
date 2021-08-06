78803e1cd086cc9929079f53956e9e11b7b0d2c2
Shoreniko <sjanj19@freeuni.edu.ge>
2021-08-06 16:20:25 +0400

    added a new field to title model class

--- src/main/java/Models/Item.java
+++ src/main/java/Models/Item.java
@@ -3,20 +3,23 @@ package Models;
 public class Item {
     // Instance variable declarations.
     private String itemID;
+    private String title;
     private String category;
     private String uploader;
     private double score;
     private String coverURL;
 
     // Constructor.
-    public Item(String itemID, String category, String uploader, double score, String coverURL){
+    public Item(String itemID, String title, String category, String uploader, double score, String coverURL){
         this.itemID = itemID;
+        this.title = title;
         this.category = category;
         this.uploader = uploader;
         this.score = score;
         this.coverURL = coverURL;
     }
 
+    // Getter methods.
     public String getTitle(){
         // IMPLEMENT
         return null;
