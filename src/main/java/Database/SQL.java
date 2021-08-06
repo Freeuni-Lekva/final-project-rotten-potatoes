c0d6710bd64bc8d25eba93fd767398f4d762a501
Shoreniko <sjanj19@freeuni.edu.ge>
2021-08-07 03:22:51 +0400

    added SQL class that establishes a connection with the database

---
+++ src/main/java/Database/SQL.java
@@ -0,0 +1,39 @@
+package Database;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SQL implements DB {
+    // Constant variable declarations.
+    private static final String HOSTNAME = "127.0.0.1";
+    private static final String PORT = "3306";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "starwars";
+    private static final String DATABASE = "rotten_potatoes";
+
+    // Instance variable declarations.
+    private final BasicDataSource dataSource;
+    private Connection connection;
+
+    // Constructor.
+    public SQL(){
+        // Initialise the data source and set its fields.
+        dataSource = new BasicDataSource();
+        dataSource.setUrl("jdbc:mysql://" + HOSTNAME + ":" + PORT + "/" + DATABASE);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        try {
+            connection = dataSource.getConnection();
+        } catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+    }
+
+}
