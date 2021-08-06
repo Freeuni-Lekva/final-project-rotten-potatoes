09046b09643cd5761be525eb95e63442277db054
Shoreniko <sjanj19@freeuni.edu.ge>
2021-08-06 15:46:06 +0400

    added fields and constructor to items' model class

--- src/main/java/Models/Item.java
+++ src/main/java/Models/Item.java
@@ -1,6 +1,22 @@
 package Models;
 
 public class Item {
+    // Instance variable declarations.
+    private String itemID;
+    private String category;
+    private String uploader;
+    private double score;
+    private String coverURL;
+
+    // Constructor.
+    public Item(String itemID, String category, String uploader, double score, String coverURL){
+        this.itemID = itemID;
+        this.category = category;
+        this.uploader = uploader;
+        this.score = score;
+        this.coverURL = coverURL;
+    }
+
     public String getTitle(){
         // IMPLEMENT
         return null;
