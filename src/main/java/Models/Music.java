4c6650a65538ffa276764851ff1c6a697aeefbc9
Shoreniko <sjanj19@freeuni.edu.ge>
2021-08-06 18:49:05 +0400

    added getter/setter methods to music model class

--- src/main/java/Models/Music.java
+++ src/main/java/Models/Music.java
@@ -27,4 +27,86 @@ public class Music {
         this.score = score;
         this.numOfReviews = numOfReviews;
     }
+
+    // Getter methods.
+    public String getMusicID(){
+        return musicID;
+    };
+
+    public String getArtist(){
+        return artist;
+    };
+
+    public String getTitle(){
+        return title;
+    };
+
+    public String getLabel(){
+        return label;
+    };
+
+    public int getReleaseYear(){
+        return releaseYear;
+    };
+
+    public String getGenre(){
+        return genre;
+    };
+
+    public String getAlbumCoverURL(){
+        return albumCoverURL;
+    };
+
+    public String getUploader(){
+        return uploader;
+    };
+
+    public double getScore(){
+        return score;
+    };
+
+    public int getNumOfReviews(){
+        return numOfReviews;
+    };
+
+    // Setter methods.
+    public void setMusicID(String musicID){
+        this.musicID = musicID;
+    };
+
+    public void setArtist(String artist){
+        this.artist = artist;
+    };
+
+    public void setTitle(String title){
+        this.title = title;
+    };
+
+    public void setLabel(String label){
+        this.label = label;
+    };
+
+    public void setReleaseYear(int releaseYear){
+        this.releaseYear = releaseYear;
+    };
+
+    public void setGenre(String genre){
+        this.genre = genre;
+    };
+
+    public void setAlbumCoverURL(String albumCoverURL){
+        this.albumCoverURL = albumCoverURL;
+    };
+
+    public void setUploader(String uploader){
+        this.uploader = uploader;
+    };
+
+    public void setScore(double score){
+        this.score = score;
+    };
+
+    public void setNumOfReviews(int numOfReviews){
+        this.numOfReviews = numOfReviews;
+    };
 }
