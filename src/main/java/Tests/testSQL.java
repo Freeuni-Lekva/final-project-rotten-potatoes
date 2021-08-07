52e25b93f4a615c69b8b23b1c13e1ad56a99a337
Shoreniko <sjanj19@freeuni.edu.ge>
2021-08-07 04:22:32 +0400

    made a shallow check on whether "where clause" selection statement works
    conditionSelect ფუნქციის გამოყენებისას სწორად უნდა შევარჩიოთ პარამეტრები. კომენტარები ამასთან დაკავშირებით შესაბამის ტესტებთან დავწერე.

--- src/main/java/Tests/testSQL.java
+++ src/main/java/Tests/testSQL.java
@@ -3,17 +3,29 @@ package Tests;
 import Database.DB;
 import Database.SQL;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 public class testSQL {
+    private DB db;
+
+    /* რატომღაც არ მუშაობს, მოგვიანებით მივხედავ.
+    @BeforeAll
+    public void init(){
+        db = new SQL();
+    } */
+
     // insert ოპერაციების სისწორის შემოწმება უკეთ მოხდება, როცა search-ის იმპლემენტაციაც მოხდება. ამჟამად, მთავარი მიზანია,
     // რომ დაინახოთ, როგორ ხდება insert ოპერაციაში პარამეტრების გადაცემა.
     @Test
     public void testInsert(){
-        DB db = new SQL();
-
+        db = new SQL();
         // სხვადასხვა ცხრილებზე insert ოპერაციის გაშვების მაგალითები. დააკვირდით, რომ TEXT, CHAR(n), DATE და ა.შ. ტიპის
         // სვეტების შესაბამისი მნიშვნელობები დამატებით მოქცეულია ერთხაზიან ბრჭყალებში ('...'), ხოლო ასეთი ბრჭყალები აღარ
         // სჭირდება, მაგ; INT, DOUBLE და ა.შ. ტიპის სვეტების შესაბამის მნიშვნელობებს. model კლასების ფუნქციების იმპლემენტაციის
@@ -31,5 +43,24 @@ public class testSQL {
         db.insert("ITEMS", item1);
         db.insert("ITEMS", item2);
         db.insert("BOOKS", book);
+    }
+
+    @Test
+    public void testConditionedSelect() throws SQLException {
+        db = new SQL();
+        int numOfRowsReturned = 0;
+
+        // დააკვირდით, რომ conditionedSelect ფუნქციას სწორად გადასცეთ მესამე პარამეტრი. აქაც მაღლა აღწერილი სიტუაციის
+        // ანალოგიური შემთხვევაა ერთხაზიან ბრჭყალებზე (მაგ: ...where name = 'admin', მაგრამ ...where age = 5).
+        ResultSet user = db.conditionedSelect("USERS", "username", "'admin'");
+        while(user.next()){
+            numOfRowsReturned++;
+            assertEquals("admin", user.getString("username"));
+            assertEquals("Giorgi", user.getString("first_name"));
+            assertEquals("Meore", user.getString("last_name"));
+            assertEquals("1900-03-07", user.getString("date_of_birth"));
+            assertEquals("RottenPotatoes", user.getString("hash_password"));
+        }
+        assertEquals(1, numOfRowsReturned);
     }
 }
