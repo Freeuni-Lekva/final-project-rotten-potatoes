ca4bf7c969675bf1ec638ba443404ca6ffeb4430
Shoreniko <sjanj19@freeuni.edu.ge>
2021-08-12 15:01:32 +0400

    added servlet to redirect admin from profile page to report page

---
+++ src/main/java/Servlets/ReportServlet.java
@@ -0,0 +1,19 @@
+package Servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ReportServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        httpServletRequest.getRequestDispatcher("/WEB-INF/report.jsp").forward(httpServletRequest, httpServletResponse);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        httpServletRequest.getRequestDispatcher("/WEB-INF/report.jsp").forward(httpServletRequest, httpServletResponse);
+    }
+}
