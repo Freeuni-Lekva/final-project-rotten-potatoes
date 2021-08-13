63a8037599fa2dc324b1e976440b3c451a32a161
qeto22 <83922302+qeto22@users.noreply.github.com>
2021-08-13 20:26:58 +0400

    Added and implemented some methods to Notification class

--- src/main/java/Models/Notification.java
+++ src/main/java/Models/Notification.java
@@ -1,12 +1,23 @@
 package Models;
 
+import Database.DB;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Notification {
+
+    private static final String NOTIFICATIONS_TABLE = "NOTIFICATIONS";
 
     private int notificationID;
     private String receiverUsername;
     private String senderUsername;
     private String itemId;
     private String notificationType;
+
 
     public Notification(int notificationID, String receiverUsername, String senderUsername, String itemId, String notificationType) {
         this.notificationID = notificationID;
@@ -16,6 +27,24 @@ public class Notification {
         this.notificationType = notificationType;
     }
 
+    public static int addNotification(DB db ,String receiverUsername, String senderUsername , String itemId , String notificationType){
+        return db.insert(NOTIFICATIONS_TABLE , new ArrayList<>(Arrays.asList(Item.surroundWithSingleQuotes(receiverUsername)
+                ,Item.surroundWithSingleQuotes(senderUsername) , Item.surroundWithSingleQuotes(itemId)
+                ,Item.surroundWithSingleQuotes(notificationType))));
+    }
+
+    public static List<Notification> getNotifications(DB db, String username) throws SQLException {
+        List<Notification> notifications = new ArrayList<>();
+        ResultSet notifications_rows = db.conditionedSelect(NOTIFICATIONS_TABLE , "receiver_username"
+                , Item.surroundWithSingleQuotes(username));
+        while (notifications_rows.next()){
+            Notification notification = new Notification(notifications_rows.getInt(1)
+                    , notifications_rows.getString(2) , notifications_rows.getString(3)
+                    ,notifications_rows.getString(4) , notifications_rows.getString(5));
+            notifications.add(notification);
+        }
+        return notifications;
+    }
 
     //getter methods
     public int getNotificationID(){
