aac24d246dc84a79505bf6b114dd9f8b4c11f1e3
Shoreniko <sjanj19@freeuni.edu.ge>
2021-08-14 20:18:44 +0400

    changed minor details in ShowReportServlet & report.jsp

--- src/main/java/Servlets/ShowReportServlet.java
+++ src/main/java/Servlets/ShowReportServlet.java
@@ -1,23 +1,26 @@
 package Servlets;
 
+import Models.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class ShowReportServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
         String reportID = httpServletRequest.getParameter("report_id");
-        httpServletRequest.setAttribute("report_id", reportID);
+        httpServletRequest.getSession().setAttribute("report_id", reportID);
         httpServletRequest.getRequestDispatcher("/WEB-INF/report.jsp").forward(httpServletRequest, httpServletResponse);
     }
 
     @Override
     protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
         String reportID = httpServletRequest.getParameter("report_id");
-        httpServletRequest.setAttribute("report_id", reportID);
+        httpServletRequest.getSession().setAttribute("report_id", reportID);
         httpServletRequest.getRequestDispatcher("/WEB-INF/report.jsp").forward(httpServletRequest, httpServletResponse);
     }
 }
