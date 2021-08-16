15118a6e48e7cdb20ba4d7ed7b9c793ade56b808
qeto22 <83922302+qeto22@users.noreply.github.com>
2021-08-16 04:03:17 +0400

    Added UserNotificationServlet

---
+++ src/main/java/Servlets/UserNotificationServlet.java
@@ -0,0 +1,29 @@
+package Servlets;
+
+import Database.DB;
+import Database.SQL;
+import Models.Notification;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserNotificationServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String notificationId = request.getParameter("notificationID");
+        DB db = new SQL();
+        db.delete("NOTIFICATIONS", "notificationID", notificationId,"notificationID", notificationId);
+        request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+    }
+}
